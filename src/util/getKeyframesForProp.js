c86eb064ec8f64868129e098d2351be68b3ded13
nascherman <n_scherman@hotmail.com>
2016-07-07 17:40:19 -0400

    converting position to expression for motion paths

--- src/util/getKeyframesForProp.js
+++ src/util/getKeyframesForProp.js
@@ -1,31 +1,38 @@
+'use strict';
+
 var getEaseForKeyFrame = require('./getEaseForKeyFrame');
 
 // this function will export keyframes for a property
 module.exports = function getKeyFramesForProp(prop) {
   var rVal = [];
-
-
+  
   // some properties don't have a value set on them
-  if(prop.propertyValueType !== PropertyValueType.NO_VALUE) {
+  if (prop.propertyValueType !== PropertyValueType.NO_VALUE) {
     // we have keyframes add all keyframes
-    if(prop.numKeys > 0) {
-
-      for(var i = 1; i <= prop.numKeys; i++) {
-        rVal.push(
-          [ prop.keyTime(i), prop.keyValue(i), getEaseForKeyFrame(prop, i) ]
-        );
+    if (prop.numKeys > 0) {
+      // Position needs to be an expression to track curved motion 
+      if(prop.name === 'Position' && prop.numKeys > 2) {
+        convertToExpression(prop);
       }
-    // we do not have keyframes just add the first
+
+      for (var i = 1; i <= prop.numKeys; i++) {
+        rVal.push([prop.keyTime(i), prop.keyValue(i), getEaseForKeyFrame(prop, i)]);
+      }
+      // we do not have keyframes just add the first
     } else {
 
       // some properties don't have this
-      if(prop.valueAtTime) {
-        rVal.push(
-          [ 0, prop.valueAtTime(0, false) ]
-        );
+      if (prop.valueAtTime) {
+        rVal.push([0, prop.valueAtTime(0, false)]);
       }
     }
   }
 
   return rVal;
+
+  function convertToExpression(prop) {
+    prop.selected = true;
+    app.executeCommand(2702);
+    app.executeCommand(2639);
+  }
 };
